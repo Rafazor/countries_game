62cf5935974991ac10b9a433717f635f833993d7
Calin Rafa <calin.rafa@evozon.com>
2019-04-18 14:44:32 +0300

    Home Screen Style.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, View, Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {persistor} from '../store/store'
 
 
@@ -13,17 +13,44 @@ class HomeScreen extends React.Component {
     render() {
         return (
             <View style={{flex: 1, justifyContent: 'center', alignContent: 'center'}}>
-                <Button
-                    title="Start Game"
-                    onPress={this.handleGameStart}
-                />
-                <Button
-                    title="Clear store"
-                    onPress={() => persistor.purge()}
-                />
+                <View style={styles.introContainer}>
+                    <Text style={styles.introText}>Can You Guess All The Capitals?</Text>
+                </View>
+                <View style={styles.startContainer}>
+                    <Text style={styles.start} onPress={this.handleGameStart}>Start Game</Text>
+                </View>
+                <View>
+                    <Text onPress={() => persistor.purge()}>Clear store</Text>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    introContainer: {
+        paddingVertical: 20,
+    },
+    introText: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 25
+    },
+    startContainer: {
+        alignSelf: 'stretch',
+        marginHorizontal: 50,
+        marginVertical: 7,
+        backgroundColor: 'red',
+        borderColor: 'blue',
+        borderWidth: 2,
+        paddingVertical: 5,
+    },
+    start: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 20,
+    }
+})
 
 export default HomeScreen
